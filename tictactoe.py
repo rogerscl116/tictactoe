8b0b80dc9f7f0d2a5c17e796590e44e823db81d7
Charity Rogers <rogerscl116@gmail.com>
2022-05-29 22:17:15 -0500

    create board and take player input

---
+++ tictactoe.py
@@ -0,0 +1,25 @@
+board = ["-", "-", "-",
+        "-", "-", "-",
+        "-", "-", "-"]
+currentPlayer = "X"
+winner = None
+gameRunning = True
+
+# print game board
+def printBoard(board):
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print("---------")
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print("---------")
+    print(board[6] + " | " + board[7] + " | " + board[8])
+
+# take player input
+def playerInput(board):
+    inp = int(input("Enter a number 1-9: "))
+    if inp >= 1 and inp <= 9 and board[inp-1] == "-":
+        board[inp-1] = currentPlayer
+# check for win or tie
+
+# switch the player
+
+# check for win or tie again
