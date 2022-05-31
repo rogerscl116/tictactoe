17b4547cfe10e43f24b40034be2a6a1825b1332c
Charity Rogers <rogerscl116@gmail.com>
2022-05-30 22:07:21 -0500

    check for win or tie and switch the player

--- tictactoe.py
+++ tictactoe.py
@@ -19,14 +19,65 @@ def playerInput(board):
     if inp >= 1 and inp <= 9 and board[inp-1] == "-":
         board[inp-1] = currentPlayer
     else:
-        print("Oops, This spot is already taken!")
+        print("Oops, this spot is already taken!")
             
 # check for win or tie
+def checkHorizonal(board):
+    global winner
+    if board[0] == board[1] == board[2] and board[0] != "-":
+        winner = board[0]
+        return True
+    elif board[3] == board[4] == board[5] and board[3] != "-":
+        winner = board[3]
+        return True
+    elif board[6] == board[7] == board[8] and board[6] != "-":
+        winner = board[6]
+        return True
+
+def checkVertical(board):
+    global winner
+    if board[0] == board[3] == board[6] and board[0] != "-":
+        winner = board[0]
+        return True
+    elif board[1] == board[4] == board[7] and board[1] != "-":
+        winner = board[1]
+        return True
+    elif board[2] == board[5] == board[8] and board[2] != "-":
+        winner = board[2]
+        return True
+
+def checkDiag(board): 
+    global winner
+    if board[0] == board[4] == board[8] and board[0] != "-":
+        winner = board[0]
+        return True
+    elif board[2] == board[4] == board[6] and board[2] != "-":
+        winner = board[2]
+        return True
+
+def checkTie(board):
+    if "-" not in board:
+        printBoard(board)
+        print("It's a tie!")
+        gameRunning = False
+
+def checkWin():
+    if checkDiag(board) or checkHorizonal(board) or checkVertical(board):
+        print(f"The winner is {winner}!")     
 
 # switch the player
+def switchPlayer():
+    global currentPlayer
+    if currentPlayer == "X":
+        currentPlayer = "O"
+    else:
+        currentPlayer = "X"
 
 # check for win or tie again
 
 while gameRunning:
     printBoard(board)
     playerInput(board)
+    checkWin()
+    checkTie(board)
+    switchPlayer()
